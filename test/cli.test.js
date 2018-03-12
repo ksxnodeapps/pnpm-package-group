858b82d34063cbffff80cc7a806b523e2bdcfdaf
khai96_ <hvksmr1996@gmail.com>
2018-03-12 16:38:17 +0700

    Update cli.test.js

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,5 +1,13 @@
 'use strict'
+const execa = require('execa')
+const executable = require.resolve('../index.js')
 
-it('should match snapshot', () => {
-  // ok...
+const trackSpawnSnap = (argv = [], options) => () => {
+  const {code: status, stdout, stderr} = execa.sync(executable, argv, options)
+  expect({status, stdout, stderr}).toMatchSnapshot()
+}
+
+describe('program', () => {
+  it('being invoked without arguments', trackSpawnSnap())
+  it('--help', trackSpawnSnap(['--help']))
 })
