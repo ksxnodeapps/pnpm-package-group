9518a8589cd3912565e295bbe441d8042283beb2
khai96_ <hvksmr1996@gmail.com>
2018-03-12 15:17:44 +0700

    Track arguments

---
+++ test/arguments/data.test.js
@@ -0,0 +1,19 @@
+'use strict'
+const {resolve} = require('path')
+const {readFileSync} = require('fs')
+
+const getText = basename =>
+  readFileSync(resolve(__dirname, '../../lib/arguments', basename), 'utf8')
+
+const yaml = require('js-yaml').safeLoad(getText('data.yaml'))
+const json = JSON.parse(getText('data.json'))
+
+describe('data.yaml and data.json', () => {
+  it('should stays unchanged', () => {
+    expect({yaml, json}).toMatchSnapshot()
+  })
+
+  it('should be equal', () => {
+    expect(yaml).toEqual(json)
+  })
+})
